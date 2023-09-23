e7e96657a254c624a56d35df759a26d9ce9249ea
Elite <casulemarc2014@gmail.com>
2023-09-23 01:05:40 -0400

    Update template form - complete

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  submit(loginForm: any) {
+    console.log('Pressed submit for ', loginForm);
+  }
+}
